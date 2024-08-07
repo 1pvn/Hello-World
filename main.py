d3e293aaf5f02e82283b35107535a1945a338334
ipvn <166346249+1pvn@users.noreply.github.com>
2024-08-07 07:27:02 -0400

    Add files via upload
    print command
    :D

---
+++ main.py
@@ -0,0 +1,6 @@
+print("hello, world🌏")
+
+# This is a Python print command.
+# It's pretty simple.
+# The words inside the parentheses and double quotes 
+# will be displayed on the console when you run the code.
